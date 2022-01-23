4acc7203841af405b5b34710d3daa1664fa18d47
Hitesh Saini <hiteshsaini57520@gmail.com>
2022-01-23 19:39:56 +0530

    file updated

--- crowderabackend/backend/models.py
+++ crowderabackend/backend/models.py
@@ -5,7 +5,12 @@ class Movies(models.Model):
     name=models.CharField(max_length=20)
     duration=models.IntegerField()
     id=models.AutoField(primary_key=True)
+    image=models.URLField()
 
     def __str__(self):
         self.name
+
+class Cast(models.Model):
+    castName=models.CharField(max_length=20)
+    des=models.TextField()
     
--- crowderabackend/backend/serializers.py
+++ crowderabackend/backend/serializers.py
@@ -6,3 +6,8 @@ class MovieSerializers(serializers.ModelSerializer):
     class Meta:
         model=models.Movies
         fields='__all__'
+
+class CastSerializers(serializers.ModelSerializer):
+    class Meta:
+        model=models.Cast
+        fields='__all__'
