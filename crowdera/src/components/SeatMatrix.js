041482316c9774882553157191eac4b79fd9d652
Hitesh Saini <hiteshsaini57520@gmail.com>
2022-01-24 19:44:54 +0530

    file added

---
+++ crowdera/src/components/SeatMatrix.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Seat from './Seat'
+import './styles/Seat.css'
+
+const GenerateSeats = (seatNumbers) => {
+	return (
+		<div className="row">
+			{
+				seatNumbers.map((seatNumber) => {
+					return <Seat seatno={seatNumber} key={seatNumber}/>
+				})
+			}
+		</div>
+	)
+}
